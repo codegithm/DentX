2c20837da06e6fddaf60ca343c1e2d997e3e1c44
Siviwe <152187964+siviwe-asthat@users.noreply.github.com>
2024-05-04 13:09:52 +0200

    new changes

--- script.js
+++ script.js
@@ -24,4 +24,30 @@ $(document).ready(function () {
         alert("Oops... " + JSON.stringify(error));
       });
   });
+
+  $("form[name='contact']").submit(function (e) {
+    e.preventDefault();
+    var formsubmit = document.getElementById("call-us-form");
+    var formData = new FormData(formsubmit);
+    formData.append("service_id", "service_laeyc94");
+    formData.append("template_id", "template_lh2orkr");
+    formData.append("user_id", "URj1ren_ccMcAJvfT");
+
+    alert("Sending!");
+    setTimeout(function () {
+      $(".alert").alert("close");
+    }, 300);
+    $.ajax("https://api.emailjs.com/api/v1.0/email/send-form", {
+      type: "POST",
+      data: formData,
+      contentType: false, // auto-detection
+      processData: false, // no need to parse formData to string
+    })
+      .done(function () {
+        alert("Your mail is sent!");
+      })
+      .fail(function (error) {
+        alert("Oops... " + JSON.stringify(error));
+      });
+  });
 });
